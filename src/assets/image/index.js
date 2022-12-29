f519c25a258c2721475a79fb4fe6567237cf7bbb
이지호 <cctv1998@naver.com>
2022-12-29 18:30:55 +0900

    feat: 이미지 추가

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as KakaoLogin } from "./kakao_login_large_wide.png";
 export { default as Example } from "./exampleImg.png";
+export { default as Example2 } from "./example2.png";
