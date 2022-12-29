2a5fda2aaf7df90a1263f8039d9c641d62d2a247
이지호 <cctv1998@naver.com>
2022-12-29 21:58:43 +0900

    feat: 충치 입 건강 정보 페이지

--- src/lib/route/Route.jsx
+++ src/lib/route/Route.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Root, LandingPage, LoginPage } from "../../pages";
+import DetailPage from "../../pages/DetailPage";
 import ListPage from "../../pages/ListPage";
+import MouthInfo from "../../pages/MouthInfo";
 import OauthPage from "../../pages/OauthPage";
 // eslint-disable-next-line import/prefer-default-export
 export const router = createBrowserRouter([
@@ -12,6 +14,8 @@ export const router = createBrowserRouter([
       { path: "/login", element: <LoginPage /> },
       { path: "/login/oauth", element: <OauthPage /> },
       { path: "/list", element: <ListPage /> },
+      { path: "/detail/:id", element: <DetailPage /> },
+      { path: "/info/mouse", element: <MouthInfo /> },
       { path: "*", element: <Navigate replace to={"/"} /> },
     ],
   },
