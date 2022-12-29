c9c5a39f7fbc5884496fa5d6756b6fd7e60d044a
이지호 <cctv1998@naver.com>
2022-12-30 00:15:03 +0900

    feat: 설명 페이지 공통 컨테이너 작성

---
+++ src/lib/styles/commonStyles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+// eslint-disable-next-line import/prefer-default-export
+export const DescriptionContainer = styled.div`
+  width: 1040px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+`;
